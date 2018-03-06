bbd4f741df19d42237c3647d48adab0db5cce22d
tbright17 <mingtu@asu.edu>
2018-03-06 12:33:59 -0700

    Add first_cyclic_node.py

--- linkedlist/first_cyclic_node.py
+++ linkedlist/first_cyclic_node.py
@@ -3,6 +3,20 @@
 # 1 -> 2 -> 3 -> 4 -> 5 -> 1  => 1
 # A -> B -> C -> D -> E -> C  => C
 
-def firstCyclicNode():
-    pass
+def firstCyclicNode(Node):
+    if not Node:
+        return Node
+
+    walker, runner, entry = Node, Node, Node
+    while walker.next and runner.next.next:
+        walker = walker.next
+        runner = runner.next.next
+        if walker == runner:
+            while (walker != entry):
+                walker = walker.next
+                entry = entry.next
+            return entry
+
+    return None
+
 
